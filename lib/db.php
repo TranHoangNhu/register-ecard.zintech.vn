ae0c60c67d472f08c7c0c4d6ed5c994288f23e89
Trần Hoàng Nhu <31412677+TranHoangNhu@users.noreply.github.com>
2022-11-28 11:31:13 +0700

    Update db.php

--- lib/db.php
+++ lib/db.php
@@ -1,4 +1,4 @@
+<?php
 ini_set('mssql.charset', 'UTF-8');
-
 $conn = new PDO('odbc:Driver=FreeTDS; Server=14.241.251.3; Port=14335; Database=DEMO_ECARD; TDS_Version=8.0; Client Charset=UTF-8', 'sa', 'P@ssw0rd12345');
 ?>
